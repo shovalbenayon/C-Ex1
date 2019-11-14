99763c13b65e05ed21fb77ed7d9da73aa2266f00
shovalbenayon <shovalbb14@gmail.com>
2019-11-15 00:04:51 +0200

    main change

--- main.c
+++ main.c
@@ -16,9 +16,9 @@ int main()
 	double fx1 =(sub(add(Exp(x) , Pow(x , 3)) , 2));
 	double fx2 =(add(mul(x , 3) , mul(Pow(x , 2) , 2)));
 	double fx3 =(sub(div(mul(Pow(x , 3) , 4) , 5 ) , mul(x , 2)));
-	printf("THE VALUE OF f(x) = e^x + x^3 - 2 at the point %lf is: %lf \n", x, fx1);
-	printf("THE VALUE OF f(x) = 3x + 2x^2 at the point %lf is: %lf\n", x , fx2);
-	printf("THE VALUE OF f(x) = (4x^3)/5 - 2x at the point %lf is: %lf\n", x , fx3);
+	printf("THE VALUE OF f(x) = e^x + x^3 - 2 at the point %lf is: %0.4lf \n", x, fx1);
+	printf("THE VALUE OF f(x) = 3x + 2x^2 at the point %lf is: %0.4lf\n", x , fx2);
+	printf("THE VALUE OF f(x) = (4x^3)/5 - 2x at the point %lf is: %0.4lf\n", x , fx3);
 
 }
 

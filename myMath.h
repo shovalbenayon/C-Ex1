d565a09e5b9231583ef045b11cbac40f428a1527
shovalbenayon <shovalbb14@gmail.com>
2019-11-15 00:18:16 +0200

    update

--- myMath.h
+++ myMath.h
@@ -1,5 +1,4 @@
-#ifndef MYMATH_LIBRARY_H
-#define MYMATH_LIBRARY_H
+
 
 float add(float x, float y);
 float sub(float x, float y);
@@ -8,4 +7,4 @@ double div(double x , int y);
 
 double Exp(int x);
 double Pow(double x , int y);
-#endif //MYMATH_LIBRARY_H
+
